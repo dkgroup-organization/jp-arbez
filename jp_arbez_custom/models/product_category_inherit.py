f4e3cb2309b7e9ad48952e36156d61007e3f818f
aminesoulaymani <s.amine.impactnet@gmail.com>
2021-07-01 17:00:40 +0200

    Update product_category_inherit.py

--- jp_arbez_custom/models/product_category_inherit.py
+++ jp_arbez_custom/models/product_category_inherit.py
@@ -5,10 +5,4 @@ class ProductCategoryInherit(models.Model):
     _inherit = "product.category"
     default_code = fields.Char("Référence")
     
-class ProductTemplateInherit(models.Model):
-    _inherit = "product.template"
-    barcode = fields.Char(compute="_compute_barcode")
-    
-    def _compute_barcode(self):
-        for template in self:
-            template.barcode = default_code
+
